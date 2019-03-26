f6b560bd83faa2adf26d2bae79299d9d41e3a87b
aifusenno1 <aifusenno1@gmail.com>
2019-03-26 00:22:41 -0500

    lab 4 initial commit

---
+++ Lab4/src/FIFO.c
@@ -0,0 +1,103 @@
+#include "FIFO.h"
+#include "LED.h"
+#include "OS.h"
+
+// Two-index implementation of the transmit FIFO
+// can hold 0 to TXFIFOSIZE elements
+#define TXFIFOSIZE 16 // must be a power of 2
+#define TXFIFOSUCCESS 1
+#define TXFIFOFAIL    0
+
+
+unsigned long volatile TxPutI;// put next
+unsigned long volatile TxGetI;// get next
+txDataType static TxFifo[TXFIFOSIZE];
+
+// initialize index FIFO
+void TxFifo_Init(void){ long sr;
+  sr = StartCritical(); // make atomic
+  TxPutI = TxGetI = 0;  // Empty
+  EndCritical(sr);
+}
+// add element to end of index FIFO
+// return TXFIFOSUCCESS if successful
+int TxFifo_Put(txDataType data){
+  OS_DisableInterrupts();
+  if((TxPutI-TxGetI) & ~(TXFIFOSIZE-1)){
+	OS_EnableInterrupts();
+    return(TXFIFOFAIL); // Failed, fifo full
+  }
+  TxFifo[TxPutI&(TXFIFOSIZE-1)] = data; // put
+  TxPutI++;  // Success, update
+  OS_EnableInterrupts();
+  return(TXFIFOSUCCESS);
+}
+// remove element from front of index FIFO
+// return TXFIFOSUCCESS if successful
+int TxFifo_Get(txDataType *datapt){
+  if(TxPutI == TxGetI ){
+    return(TXFIFOFAIL); // Empty if TxPutI=TxGetI
+  }
+  *datapt = TxFifo[TxGetI&(TXFIFOSIZE-1)];
+  TxGetI++;  // Success, update
+  return(TXFIFOSUCCESS);
+}
+// number of elements in index FIFO
+// 0 to TXFIFOSIZE-1
+unsigned short TxFifo_Size(void){
+ return ((unsigned short)(TxPutI-TxGetI));
+}
+
+// Two-pointer implementation of the receive FIFO
+// can hold 0 to RXFIFOSIZE-1 elements
+#define RXFIFOSIZE 16 // can be any size
+#define RXFIFOSUCCESS 1
+#define RXFIFOFAIL    0
+
+rxDataType volatile *RxPutPt; // put next
+rxDataType volatile *RxGetPt; // get next
+rxDataType static RxFifo[RXFIFOSIZE];
+
+// initialize pointer FIFO
+void RxFifo_Init(void){ long sr;
+  sr = StartCritical();      // make atomic
+  RxPutPt = RxGetPt = &RxFifo[0]; // Empty
+  EndCritical(sr);
+}
+// add element to end of pointer FIFO
+// return RXFIFOSUCCESS if successful
+int RxFifo_Put(rxDataType data){
+  rxDataType volatile *nextPutPt;
+  nextPutPt = RxPutPt+1;
+  if(nextPutPt == &RxFifo[RXFIFOSIZE]){
+    nextPutPt = &RxFifo[0];  // wrap
+  }
+  if(nextPutPt == RxGetPt){
+    return(RXFIFOFAIL);      // Failed, fifo full
+  }
+  else{
+    *(RxPutPt) = data;       // Put
+    RxPutPt = nextPutPt;     // Success, update
+    return(RXFIFOSUCCESS);
+  }
+}
+// remove element from front of pointer FIFO
+// return RXFIFOSUCCESS if successful
+int RxFifo_Get(rxDataType *datapt){
+  if(RxPutPt == RxGetPt ){
+    return(RXFIFOFAIL);      // Empty if PutPt=GetPt
+  }
+  *datapt = *(RxGetPt++);
+  if(RxGetPt == &RxFifo[RXFIFOSIZE]){
+     RxGetPt = &RxFifo[0];   // wrap
+  }
+  return(RXFIFOSUCCESS);
+}
+// number of elements in pointer FIFO
+// 0 to RXFIFOSIZE-1
+unsigned short RxFifo_Size(void){
+  if(RxPutPt < RxGetPt){
+    return ((unsigned short)(RxPutPt-RxGetPt+(RXFIFOSIZE*sizeof(rxDataType)))/sizeof(rxDataType));
+  }
+  return ((unsigned short)(RxPutPt-RxGetPt)/sizeof(rxDataType));
+}
---
+++ Lab4/src/interpreter.c
@@ -0,0 +1,157 @@
+
+#include <string.h>
+#include <stdlib.h>     /* atoi */
+#include "ST7735.h"
+#include "Serial.h"
+#include "LED.h"
+#include "OS.h"
+
+
+static void parse_lcd(char cmd[][20], int len);
+static void parse_led(char cmd[][20], int len);
+void parse_jitter(char cmd[][20], int len);
+char input[200];
+
+void interpreter(void) {
+	while (1) {
+		Serial_printf("$ ");
+
+		Serial_InString(input, 30);  // 200 will not work for some reason
+
+		Serial_println("");
+
+		char *c = &input[0];
+
+		char command[10][20];
+		memset(command, '\0', sizeof(command));
+
+		int len = 0; // number of tokens in command
+		int i = 0;
+
+//		OS_DisableInterrupts();
+		while (*c != '\0') {
+			if (*c == '"') {
+				c++;
+				while (*c != '"') {
+					if (*c == '\0') {
+//						OS_EnableInterrupts();
+						Serial_println("Closing quote not found.");
+						goto END_OF_LOOP;  // due to error, jump out of the current input
+					}
+					command[len][i++] = *c++;
+				}
+			}
+			else if (*c == ' ' || *c == '\t') {
+				if (i) {  // if i = 0, don't end since no argument
+					command[len++][i] = '\0';
+					i = 0;
+				}
+			}
+			else {  // add to the current token
+				command[len][i++] = *c;
+			}
+			c++;
+		}
+//		OS_EnableInterrupts();
+
+		if (command[0][0] == '\0') {
+			goto END_OF_LOOP;  // no input
+		}
+
+		command[len++][i] = '\0';   // terminate the final token
+		command[len][0] = '\0';     // terminate the array with a NULL
+
+//		 lcd output
+		if (strcmp(command[0], "lcd") == 0) {
+			parse_lcd(command, len);
+
+		}
+//		 led
+		if (strcmp(command[0], "led") == 0) {
+			parse_led(command, len);
+		}
+
+		if (strcmp(command[0], "jitter") == 0) {
+			parse_jitter(command, len);
+		}
+
+		else {
+			Serial_println("Unrecognized command.");
+		}
+		END_OF_LOOP : ;
+	}
+}
+
+/*
+ *
+ * len: number of the arguments
+ */
+
+static void parse_lcd(char cmd[][20], int len) {
+//	Serial_OutUDec(len);
+//	long sr = StartCritical();
+	if (len == 1) {
+		Serial_println("lcd: need at least an argument.");
+		return;
+	}
+
+	if (!strcmp(cmd[1], "message")) {
+		if (len < 5) {
+			Serial_println("lcd message: insufficient arguments.");
+			return;
+		}
+
+		int device = atoi(cmd[2]);
+		int line = atoi(cmd[3]);
+
+		// if device arg is not an integer
+		if ((strlen(cmd[2]) != 1 || cmd[2][0] != '0') && device == 0) {
+			Serial_println("lcd message: incorrect arguments.");
+			return;
+		}
+
+		if ((strlen(cmd[3]) != 1 || cmd[3][0] != '0') && line == 0) {
+			Serial_println("lcd message: incorrect arguments.");
+			return;
+		}
+
+		ST7735_Message(device, line, cmd[4], 0);
+	}
+	else {
+		Serial_println("Unrecognized argument.");
+	}
+//	EndCritical(sr);
+}
+
+
+
+static void parse_led(char cmd[][20], int len) {
+//	long sr = StartCritical();
+	if (len == 1) {
+		Serial_println("led: need at least an argument.");
+		return;
+	}
+
+	if (!strcmp(cmd[1], "r")) {
+		LED_RED_TOGGLE();
+	} else if (!strcmp(cmd[1], "g")) {
+		LED_GREEN_TOGGLE();
+	} else if (!strcmp(cmd[1], "b")) {
+		LED_BLUE_TOGGLE();
+	}
+	else {
+		Serial_println("Unrecognized argument.");
+	}
+//	EndCritical(sr);
+}
+
+
+extern unsigned long jitter1Histogram[JITTERSIZE];
+
+void parse_jitter(char cmd[][20], int len) {
+	for (int i =0; i < JITTERSIZE; i++) {
+		Serial_println("%u %u", i, jitter1Histogram[i] );
+	}
+
+}
+
